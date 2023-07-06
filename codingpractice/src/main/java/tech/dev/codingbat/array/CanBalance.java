1d5482f852a36147349dd9a811fa43d010774b9f
ALAM <mdmurad.alam2@capgemini.com>
2023-07-06 10:28:10 +0200

    CanBalance there is a place to split array

--- codingpractice/src/main/java/tech/dev/codingbat/array/CanBalance.java
+++ codingpractice/src/main/java/tech/dev/codingbat/array/CanBalance.java
@@ -2,6 +2,54 @@ package tech.dev.codingbat.array;
 
 public class CanBalance {
 
+  public CanBalance() {}
+
+  public boolean canBalance(int[] nums) {
+
+    int i=0;
+    int j= nums.length-1;
+    Boolean isPlaceToSplit = false;
+
+    int sum1 = nums[i];
+    int sum2 = nums[j];
+
+    while((j-i)>=1) {
+
+      if(sum1==sum2 && (j-i)!=1){
+          i=i+1;
+          sum1=sum1+nums[i];
+          j=j-1;
+          sum2=sum2+nums[j];
+
+      }
+
+      if(sum1==sum2 && (j-i)==1){
+        isPlaceToSplit = true;
+        break;
+      }
+
+      if((sum1<sum2 || sum2<sum1) && (j-i)==1){
+        break;
+      }
+
+      if(sum1<sum2 && j-i!=1){
+        i=i+1;
+        sum1=sum1+nums[i];
+
+      }
+
+      if(sum2<sum1 && j-i!=1){
+        j=j-1;
+        sum2=sum2+nums[j];
+
+      }
+    }
+
+    return isPlaceToSplit;
+
+
+}
+
 
 
 }
---
+++ codingpractice/src/test/java/tech/dev/codingbat/array/CanBalanceTests.java
@@ -0,0 +1,47 @@
+package tech.dev.codingbat.array;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CanBalanceTests {
+  private CanBalance canBalance;
+
+  public static int[][] splitData() {
+    return new int[][] { { 1, 1, 1, 2, 1 }, {10, 10}, { 10, 0, 1, -1, 10 },
+                         {1, 1, 1, 1, 4},{1, 2, 3, 1, 0, 2, 3} };
+  }
+
+  public static int[][] nonSplitData() {
+    return new int[][] { { 2, 1, 1, 2, 1 }, {2, 1, 1, 1, 4}, { 2, 3, 4, 1, 2 },
+                         {1, 2, 3, 1, 0, 1, 3}, {1} };
+}
+
+
+
+  @BeforeEach
+  public void setUp() {
+    canBalance = new CanBalance();
+  }
+
+
+  @ParameterizedTest
+  @MethodSource(value =  "splitData")
+  public void there_is_a_place_to_balance_the_given_array_by_split(int[] splitdata) {
+
+    assertEquals(true, canBalance.canBalance(splitdata) );
+  }
+
+
+  @ParameterizedTest
+  @MethodSource(value =  "nonSplitData")
+  public void there_is_no__place_to_balance_the_given_array_by_split(int[] nonSplitData) {
+
+    assertEquals(false, canBalance.canBalance(nonSplitData) );
+  }
+
+
+
+}
